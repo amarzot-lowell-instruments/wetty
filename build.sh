b9c822fd17e4bb3dad15def96463941c50bc7477
Alexander Marzot <alec@lowellinstruments.com>
2024-07-01 21:40:50 -0400

    Use pnpm instead of npm in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-npm x tsc -- -p tsconfig.node.json 
-npm x tsc -- -p tsconfig.browser.json
-npm x esbuild -- src/client/wetty.ts --bundle --platform=browser --sourcemap --minify --outdir=build/client $@
-npm x sass -- src/assets/scss:build/assets/css --load-path=node_modules -s compressed --no-source-map
+pnpm exec tsc -p tsconfig.node.json 
+pnpm exec tsc -p tsconfig.browser.json
+pnpm exec esbuild src/client/wetty.ts --bundle --platform=browser --sourcemap --minify --outdir=build/client $@
+pnpm exec sass src/assets/scss:build/assets/css --load-path=node_modules -s compressed --no-source-map
 cp -r src/assets/xterm_config src/assets/favicon.ico build/assets
